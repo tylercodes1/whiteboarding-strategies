5581cef4f5ad3872cd58e6e6de9a6f9a0bd2adef
tkmoney11 <tylerkim@outlook.com>
2021-04-10 14:26:29 -0700

    Added SlidingWindowPage.css

--- src/pages/SlidingWindowPage/SlidingWindowPage.js
+++ src/pages/SlidingWindowPage/SlidingWindowPage.js
@@ -1,3 +1,9 @@
+import "./SlidingWindowPage.css";
+
 export default function SlidingWindowPage() {
-	return <div className="sliding-window-page">Sliding Window</div>;
+	return (
+		<div className="sliding-window-page">
+			<div>Sliding Window</div>
+		</div>
+	);
 }
