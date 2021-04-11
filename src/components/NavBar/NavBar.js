8f1b2f1a2b8b5eb42fda31c279182b32bd5a2a5b
tkmoney11 <tylerkim@outlook.com>
2021-04-10 22:13:22 -0700

    Wider tab, more options

--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -5,7 +5,7 @@ export default function NavBar() {
 	return (
 		<div className="navbar">
 			<div className="dropdown">
-				<div>Pages</div>
+				<div>Strategies</div>
 				<div className="dropdown-content">
 					<NavLink to="/" className="nav-links">
 						Home
